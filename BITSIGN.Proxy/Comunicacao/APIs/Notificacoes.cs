de18deaa934526ce843bf94d1b3a1035c9320216
Israel Aece <israelaece@yahoo.com.br>
2021-03-03 21:19:18 -0300

    Ajuste na documentação.

--- BITSIGN.Proxy/Comunicacao/APIs/Notificacoes.cs
+++ BITSIGN.Proxy/Comunicacao/APIs/Notificacoes.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace BITSIGN.Proxy.Comunicacao.APIs
 {
     /// <summary>
-    /// Manipulação e informações sobre as notificações e callbacks gerados pelo sistema.
+    /// Informações e manipulação de notificações e callbacks.
     /// </summary>
     public class Notificacoes : API
     {
