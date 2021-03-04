0bd984bcb034d295b0efba9338d08de7c3d5f754
Israel Aece <israelaece@yahoo.com.br>
2021-03-04 09:29:31 -0300

    Dispose no handler.

--- BITSIGN.Proxy/Comunicacao/RastreioDaRequisicao.cs
+++ BITSIGN.Proxy/Comunicacao/RastreioDaRequisicao.cs
@@ -75,5 +75,11 @@ namespace BITSIGN.Proxy.Comunicacao
 
         private void Log(string id, Severidade severidade, string mensagem) =>
             this.logger?.Escrever(severidade, $"{id} - {mensagem}");
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            this.logger?.Dispose();
+        }
     }
 }
