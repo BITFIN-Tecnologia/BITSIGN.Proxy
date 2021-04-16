4a7d8a077567dad605d0405445836b3eaff04086
Israel Aece <israelaece@yahoo.com.br>
2021-04-16 11:16:51 -0300

    Informações sobre a certificadora.

---
+++ BITSIGN.Proxy/DTOs/Certificadora.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2021 - BITFIN Software Ltda. Todos os Direitos Reservados.
+// Código exclusivo para consumo dos serviços (APIs) da BITSIGN.
+
+namespace BITSIGN.Proxy.DTOs
+{
+    /// <summary>
+    /// Informações gerais sobre a certificadora.
+    /// </summary>
+    public class Certificadora
+    {
+        /// <summary>
+        /// Nome oficial da certificadora.
+        /// </summary>
+        public const string Nome = "BITSIGN";
+
+        /// <summary>
+        /// Endereço do site da certificadora.
+        /// </summary>
+        public const string Site = "https://www.bitsign.com.br";
+
+        /// <summary>
+        /// Endereço de e-mail da certificadora.
+        /// </summary>
+        public const string Email = "suporte@bitsign.com.br";
+    }
+}
