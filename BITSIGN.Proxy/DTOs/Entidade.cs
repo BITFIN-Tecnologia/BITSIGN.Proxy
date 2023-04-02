3b9be0deb68751da5dbd51cc2d807c786756ebfe
Israel Aece <israel.aece@bitfin.com.br>
2023-04-02 10:18:00 -0300

    Data de Nascimento/Fundação na Entidade.

--- BITSIGN.Proxy/DTOs/Entidade.cs
+++ BITSIGN.Proxy/DTOs/Entidade.cs
@@ -1,7 +1,11 @@
 ﻿// Copyright (c) 2021 - BITFIN Software Ltda. Todos os Direitos Reservados.
 // Código exclusivo para consumo dos serviços (APIs) da BITSIGN.
 
+using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Threading;
 using System.Xml.Serialization;
 
 namespace BITSIGN.Proxy.DTOs
@@ -12,6 +16,11 @@ namespace BITSIGN.Proxy.DTOs
     [DebuggerDisplay("{Nome,nq}")]
     public class Entidade : Base
     {
+        /// <summary>
+        /// Formato de serialização da data de nascimento ou fundação da entidade.
+        /// </summary>
+        public const string FormatoDaData = "yyyy-MM-dd";
+
         /// <summary>
         /// Nome completo da entidade.
         /// </summary>
@@ -35,5 +44,30 @@ namespace BITSIGN.Proxy.DTOs
         /// </summary>
         [XmlAttribute]
         public string Telefone { get; set; }
+
+        /// <summary>
+        /// Data de Nascimento (para Pessoa Física) ou Data de Fundação (para Pessoa Jurídica) em formato yyyy-MM-dd.
+        /// </summary>
+        [XmlIgnore, JsonIgnore]
+        public DateTime? DataDeNascimentoOuFundacao
+        {
+            get =>
+                string.IsNullOrWhiteSpace(this.NascimentoOuFundacao) ?
+                null :
+                DateTime.TryParseExact(
+                    this.NascimentoOuFundacao,
+                    FormatoDaData,
+                    Thread.CurrentThread.CurrentCulture,
+                    DateTimeStyles.None,
+                    out var data) ? data :
+                null;
+            set => this.NascimentoOuFundacao = value?.ToString(FormatoDaData);
+        }
+
+        /// <summary>
+        /// Data de Nascimento (para Pessoa Física) ou Data de Fundação (para Pessoa Jurídica) em formato yyyy-MM-dd.
+        /// </summary>
+        [XmlAttribute]
+        public string NascimentoOuFundacao { get; set; }
     }
 }
